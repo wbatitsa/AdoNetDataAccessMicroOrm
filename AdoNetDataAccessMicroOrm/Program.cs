df5dd72866b67937ef9e7d7a68beed27dfa9d582
Wender Robirson Batista Perez <55286372+wbatitsa@users.noreply.github.com>
2025-02-20 17:46:09 -0400

    Agrega ejemplo de lectura con ado.net

--- AdoNetDataAccessMicroOrm/Program.cs
+++ AdoNetDataAccessMicroOrm/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace AdoNetDataAccessMicroOrm
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AdoNetDataAccessMicroOrm
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var listOfProduct = new List<Product>();
+
+            var connectionString = "Server=(localdb)\\MSSQLLocalDb;Database=Northwind";
+            var connection = new SqlConnection(connectionString);
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Products";
+
+            connection.Open();
+
+            var dataReader = command.ExecuteReader();
+            while (dataReader.Read()) { 
+                var product = new Product();
+                product.ProductID = (int)dataReader["ProductID"];
+                product.ProductName = (string)dataReader["ProductName"];
+                listOfProduct.Add(product);
+            }
+            connection.Close();
+
+
+            foreach (var product in listOfProduct) {
+                Console.WriteLine(product.ProductName);
+            }
         }
+    }
+
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
     }
 }
