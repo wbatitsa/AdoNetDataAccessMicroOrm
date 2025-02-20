dd19ac99ce5fd5d5b7ea0ec4ad195ef7f61c53d1
Wender Robirson Batista Perez <55286372+wbatitsa@users.noreply.github.com>
2025-02-20 18:58:23 -0400

    transaction code with dapper

--- AdoNetDataAccessMicroOrm/Program.cs
+++ AdoNetDataAccessMicroOrm/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Net.Http.Headers;
 
 namespace AdoNetDataAccessMicroOrm
 {
@@ -11,17 +12,101 @@ namespace AdoNetDataAccessMicroOrm
             var connectionString = "Server=(localdb)\\MSSQLLocalDb;Database=Northwind";
             var connection = new SqlConnection(connectionString);
 
-            var param = new DynamicParameters(new
+            connection.Open();
+
+            var transaction = connection.BeginTransaction();
+
+            try
             {
-                ProductName = "Banana",
-                UnitPrice = 50.00
-            });
-            param.Add("@ProductID", dbType: DbType.Int32, direction: ParameterDirection.Output);
-            connection.Execute("ProductsInsert", param, commandType: CommandType.StoredProcedure);
+                // Insert order
 
-            int insertedId = param.Get<int>("@ProductID");
 
-            Console.WriteLine("Inserted id: " + insertedId);
+                //var insertOrderCommand = connection.CreateCommand();
+                //insertOrderCommand.Transaction = transaction;
+                //var insertOrderSql = @"
+                //    INSERT Orders(CustomerID, OrderDate)
+                //    VALUES(@CustomerID, @OrderDate);
+                //    SELECT SCOPE_IDENTITY();
+                //";
+                //insertOrderCommand.CommandText = insertOrderSql;
+                //insertOrderCommand.Parameters.AddWithValue("@CustomerID", "ALFKI");
+                //insertOrderCommand.Parameters.AddWithValue("@OrderDate", DateTime.UtcNow);
+                //int orderId = Convert.ToInt32(insertOrderCommand.ExecuteScalar());
+
+                // Insert order detail
+                //var insertDetailCommand = connection.CreateCommand();
+                //insertDetailCommand.Transaction = transaction;
+                //var iserrtDetailSql = @"
+                //    INSERT INTO [dbo].[Order Details]
+                //               ([OrderID]
+                //               ,[ProductID]
+                //               ,[UnitPrice]
+                //               ,[Quantity]
+                //               ,[Discount])
+                //         VALUES
+                //               (@OrderId
+                //               ,@ProductId
+                //               ,@UnitPrice
+                //               ,@Qty
+                //               ,@Discount)
+                //";
+                //insertDetailCommand.CommandText = iserrtDetailSql;
+                //insertDetailCommand.Parameters.AddWithValue("@OrderId", orderId);
+                //insertDetailCommand.Parameters.AddWithValue("@ProductId", 1);
+                //insertDetailCommand.Parameters.AddWithValue("@UnitPrice", 100);
+                //insertDetailCommand.Parameters.AddWithValue("@Qty", 10);
+                //insertDetailCommand.Parameters.AddWithValue("@Discount", 0);
+                //insertDetailCommand.ExecuteNonQuery();
+
+
+
+                var insertOrderSql = @"
+                    INSERT Orders(CustomerID, OrderDate)
+                    VALUES(@CustomerID, @OrderDate);
+                    SELECT SCOPE_IDENTITY();
+                ";
+                var orderId = connection.ExecuteScalar<int>(insertOrderSql, new
+                {
+                    CustomerID = "ALFKI",
+                    OrderDate = DateTime.UtcNow,
+                },
+                transaction: transaction);
+
+                var insertDetailSql = @"
+                    INSERT INTO [dbo].[Order Details]
+                               ([OrderID]
+                               ,[ProductID]
+                               ,[UnitPrice]
+                               ,[Quantity]
+                               ,[Discount])
+                         VALUES
+                               (@OrderId
+                               ,@ProductId
+                               ,@UnitPrice
+                               ,@Qty
+                               ,@Discount)
+                ";
+                connection.Execute(insertDetailSql, new
+                {
+                    OrderId = orderId,
+                    ProductId = 1,
+                    UnitPrice = 25,
+                    Qty = 300,
+                    Discount = 0
+                }, 
+                transaction: transaction);
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 
